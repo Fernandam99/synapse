4509b14d89a5624794269fa7542d1b378e1f8c0f
Fernandam99 <mm43651@gmail.com>
2025-08-19 22:48:56 -0500

    formularios

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ def index():
 def mision():
     return render_template("mision.html")
 
+@app.route("/login") #Decorador
+def login():
+    return render_template("login.html")
+
+@app.route("/register") #Decorador
+def register():
+    return render_template("register.html")
+
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
